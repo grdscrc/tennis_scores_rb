ccd2efaf92dcce28bcb9fc1cfd5a465df4da381d
Igor Descayrac <igor.descayrac@gmail.com>
2021-07-01 00:21:31 +0200

    Shave down tests

--- test/tennis_scores_test.rb
+++ test/tennis_scores_test.rb
@@ -2,58 +2,60 @@ require 'test/unit'
 require './src/tennis_scores.rb'
 
 class TestAdd < Test::Unit::TestCase
+    def setup
+        @scoring = TennisScores.new    
+    end
+
+    def teardown
+        assert_equal @expected, @actual
+    end
+
+    # games
     def test_score_first_point
-        scoring = TennisScores.new
-        actual = scoring.scorePoint(1)
-        expected = [[0, 0], [15, 0]]
-        assert_equal expected, actual
+        @actual = @scoring.scorePoint(1)
+        @expected = [[0, 0], [15, 0]]
     end
 
     def test_score_first_game
-        scoring = TennisScores.new
-        scoring.setScore([:adv, 40])
-        actual = scoring.scorePoint(1)
-        expected = [[1, 0], [0, 0]]
-        assert_equal expected, actual
+        @scoring.setScore([:adv, 40])
+
+        @actual = @scoring.scorePoint(1)
+        @expected = [[1, 0], [0, 0]]
     end
 
     def test_score_first_game_no_adv
-        scoring = TennisScores.new
-        scoring.setScore([15, 40])
-        actual = scoring.scorePoint(2)
-        expected = [[0, 1], [0, 0]]
-        assert_equal expected, actual
+        @scoring.setScore([15, 40])
+
+        @actual = @scoring.scorePoint(2)
+        @expected = [[0, 1], [0, 0]]
     end
 
     def test_take_adv
-        scoring = TennisScores.new
-        scoring.setScore([:adv, 15])
-        actual = scoring.scorePoint(2)
-        expected = [[0, 0], [40, 30]]
-        assert_equal expected, actual
+        @scoring.setScore([:adv, 15])
+
+        @actual = @scoring.scorePoint(2)
+        @expected = [[0, 0], [40, 30]]
     end
 
+    # sets
     def test_win_first_set
-        scoring = TennisScores.new
-        scoring.setScore([40, 15], [[5, 0]])
-        actual = scoring.scorePoint(1)
-        expected = [[6, 0], [0, 0], [0, 0]]
-        assert_equal expected, actual
+        @scoring.setScore([40, 15], [[5, 0]])
+
+        @actual = @scoring.scorePoint(1)
+        @expected = [[6, 0], [0, 0], [0, 0]]
     end
 
     def test_set_remains_if_less_than_two_games
-        scoring = TennisScores.new
-        scoring.setScore([40, 15], [[5, 5]])
-        actual = scoring.scorePoint(1)
-        expected = [[6, 5], [0, 0]]
-        assert_equal expected, actual
+        @scoring.setScore([40, 15], [[5, 5]])
+
+        @actual = @scoring.scorePoint(1)
+        @expected = [[6, 5], [0, 0]]
     end
 
     def test_win_set_by_two_games
-        scoring = TennisScores.new
-        scoring.setScore([40, :adv], [[5, 6]])
-        actual = scoring.scorePoint(2)
-        expected = [[5, 7], [0, 0], [0, 0]]
-        assert_equal expected, actual
+        @scoring.setScore([40, :adv], [[5, 6]])
+
+        @actual = @scoring.scorePoint(2)
+        @expected = [[5, 7], [0, 0], [0, 0]]
     end
 end
