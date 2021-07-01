50069bcd0b9945f406beeb60be3d6ea76ae54d6f
Igor Descayrac <igor.descayrac@gmail.com>
2021-07-01 02:11:00 +0200

    Allow exiting from prompt

--- src/tennis_prompt.rb
+++ src/tennis_prompt.rb
@@ -4,16 +4,26 @@ require './src/tennis_scores.rb'
 scoring = TennisScores.new
 
 prompt = "> "
-puts "[1] Player one scores [2] Player two scores"
+puts "[1] Player one scores / [2] Player two scores / [exit]"
 print prompt
 
-while scoring_player = gets.chomp.to_i # loop while getting user input
-    if scoring_player > 2 || scoring_player < 1
-        puts "Please input a player number"
-    else
-        score = scoring.scorePoint(scoring_player)
+loop do
+    while (input = gets.chomp) && !scoring.over?
+        break if input == 'exit'
+        scoring_player = input.to_i
+        if scoring_player > 2 || scoring_player < 1
+            puts "Please input a player number"
+        else
+            scoring.scorePoint(scoring_player)
 
-        puts scoring.display
+            puts scoring.display
+        end
+        print prompt
     end
+
+    break if input == 'exit'
+    puts "[restart] / [exit]"
     print prompt
+    input = gets.chomp
+    break if input == 'exit'
 end
